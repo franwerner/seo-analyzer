6eca2d0f91ca6d02929078952f4fb72d5d1e2b44
Franco Werner <ifrank44445@gmail.com>
2025-08-28 15:11:03 -0300

    fix(server):verificacion de urls con headers de navegador

--- repo/server/src/components/anchor.component.ts
+++ repo/server/src/components/anchor.component.ts
@@ -13,7 +13,13 @@ class AnchorComponent extends BaseComponent {
         if (!isValidHttpUrl(this.attributes.href)) return true
         try {
             const res = await fetch(this.attributes.href, {
-                method: "HEAD"
+                method: "HEAD",
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/139.0.0.0 Safari/537.36",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                    "Accept-Language": "en-US,en;q=0.9"
+                }
+
             })
             const notFound = res.status === 404
             const notResponseServer = res.status >= 500
