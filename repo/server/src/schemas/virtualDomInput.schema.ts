7436255c7441c4efcf7d80a9c492d0d6b6e5da42
Franco Werner <ifrank44445@gmail.com>
2025-09-19 13:43:19 -0300

    feat(server): Se agrega trasnformacion a url schema para autocompletado de https://

--- repo/server/src/schemas/virtualDomInput.schema.ts
+++ repo/server/src/schemas/virtualDomInput.schema.ts
@@ -1,8 +1,12 @@
 import { z } from "zod"
 
-
 const virtualDomInputSchema = z.object({
-    url: z.string().url(),
+    url: z.string().transform((val) => {
+        if (!/^https?:\/\//i.test(val)) {
+            return `https://${val}`;
+        }
+        return val;
+    })
 })
 
 export type VirtualDomInput = z.infer<typeof virtualDomInputSchema>
