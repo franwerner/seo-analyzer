a30aa34ca2cb24f67a35f50cbbe91b5e62dc08a1
Franco Werner <ifrank44445@gmail.com>
2025-10-01 11:30:53 -0300

    feat(server):Se mejora prompt de structure validation

--- repo/server/src/services/dom-validator/validations/structure.validation.ts
+++ repo/server/src/services/dom-validator/validations/structure.validation.ts
@@ -2,24 +2,26 @@ import OpenAi from "@/services/openAi.service";
 import ValidationUtility from "@/utils/validation.util";
 
 const prompt = `
-                  #RESPONDE EN INGLES
-               Eres un asistente experto en análisis estructural de HTML. Debes enfocarte únicamente en la **estructura y jerarquía de las etiquetas**, sin considerar contenido ni atributos.  
+                #RESPONDE EN INGLES
+                Eres un asistente experto en análisis estructural de HTML. Debes enfocarte únicamente en la **estructura y jerarquía de las etiquetas**,
+                sin considerar contenido ni atributos.  
 
-                 Instrucciones:  
+                #Instrucciones:  
                 1. Ignora completamente cualquier etiqueta "<script>", "<link>" y "<meta>".
                 2. Ignore contenido, atributos, text o cualquiera cosa que no sea la jerarquía de las etiquetas.
                 3. Analiza cómo está jerarquizado y construido el HTML.  
                 4. Detecta problemas de estructura como: etiquetas mal anidadas, headings fuera de orden (h1-h6), duplicados importantes, secciones faltantes, etc.  
-                5. Para cada tipo de problema, agrupa todas las instancias en un **único objeto**, usando un array de "traceIds".  
+                5. Para un mismo tipo de problema, agrupa todas las instancias en un **único objeto**, usando un array de "traceIds".  
                 6. La propiedad "tag" debe contener solo el nombre de una etiqueta en minúscula (ej. "h1", "div").  
-                7. La propiedad "message" debe ser breve, clara y explicativa. No incluyas IDs en el mensaje.  
-                8. Para elementos importantes que falten, usa "traceIds": [-0].  
-                9. Siempre entrega un **JSON estructurado**, sin comentarios ni texto adicional fuera del JSON.
+                7. La propiedad "message" debe ser breve, clara y explicativa. No incluyas IDs o nombres de etiquetas en el mensaje.  
+                8. Para elementos importantes que falten, usa "traceIds": [] vacio. 
+                9. Solo remarca los problemas que detectes, no algo para verificar que posiblemente sea un error, solo remarca errores detectados. 
+              
       
                   Ejemplo de salida:
                   [{
                   "tag": "h1",
-                  "message": "Se detectaron múltiples etiquetas h1.",
+                  "message": "Se detectaron múltiples elementos.",
                   "traceIds": [123, 456],
                   "type" : "structure"
                   }]
