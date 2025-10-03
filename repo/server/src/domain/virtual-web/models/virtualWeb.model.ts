39874ae8984ad936a1897f2f8d49f44564433b97
Franco Werner <ifrank44445@gmail.com>
2025-10-03 17:48:34 -0300

    feat(server):Se agrega virtualWeb y store

---
+++ repo/server/src/domain/virtual-web/models/virtualWeb.model.ts
@@ -0,0 +1,96 @@
+import DomValidator from "@/domain/virtual-dom/services/dom-validator";
+import OpenAi from "@/services/openAi.service";
+import PuppeterService from "@/services/puppeter.service";
+import { Tokens } from "@/types/Tokens.interface";
+import { WebSummary } from "@/types/WebSummary.interface";
+import VirtualDomStore from "../../virtual-dom/store/virtualDom.store";
+
+/**
+ * @note
+ *   Se encarga de orquestar los VirtualDom, decide que responsabildades tienen cada uno, crea los resumenes etc.
+ */
+
+
+export interface VirtualWebProps {
+    host: string
+    mainPathname: string
+    webSummary?: WebSummary | null
+}
+
+export default class VirtualWeb {
+
+    vdomStore: VirtualDomStore
+    domValidator: DomValidator
+    host: string
+    mainPathname: string
+    private webSummary?: WebSummary | null = null
+
+    constructor(
+        private openAi: OpenAi,
+        private puppeteer: PuppeterService,
+        { host, webSummary, mainPathname }: VirtualWebProps) {
+
+        this.host = host
+
+        this.mainPathname = mainPathname
+
+        this.webSummary = webSummary
+
+        this.domValidator = new DomValidator(this.openAi, this.webSummary)
+
+        this.vdomStore = new VirtualDomStore(this.puppeteer, { host, domValidator: this.domValidator })
+    }
+
+
+    setSummary(summary: WebSummary) {
+        this.webSummary = summary
+        this.domValidator.updateWebSummaryContext(summary)
+    }
+
+    async generateWebSummary(): Promise<{
+        webSummary: WebSummary,
+        tokens: Tokens
+    }> {
+
+        const snapshot = await this.vdomStore.getOrCreate(this.mainPathname).getOrGenerateSnapshot()
+
+        const texts = snapshot.vDomContext.innerTextChunks.chunks.map(chunk => chunk.parts_texts).flat()
+
+        const { response, tokens } = await this.openAi.createBasicResponse(
+            JSON.stringify(texts),
+            `
+        Instrucciones:
+        Recibirás un array de strings, cada indice del array representa una parte(oracion) del contenido de un sitio web. 
+        Tu tarea es analizar todo el contenido y generar un resumen conciso y extenso que capture la esencia del sitio.
+
+        El resumen debe incluir:
+        De qué trata el sitio web.
+
+        Principales características y elementos destacados.
+        A qué se dedica, cuál es su actividad principal o propósito.
+        Qué ofrece y para quién está dirigido.
+
+        Requisitos:
+
+        El resumen debe estar escrito en un solo bloque de texto, sin listas ni viñetas.
+        Usa un lenguaje natural y claro, pensado para análisis SEO.
+        Evita comentarios adicionales, instrucciones o explicaciones fuera del resumen.
+        El texto debe ser suficientemente descriptivo para entender el sitio en profundidad, pero sin extenderse innecesariamente.
+        `
+        )
+
+        return {
+            webSummary: {
+                summary: response,
+                generatedAt: new Date(),
+                pathnameByGeneration: this.mainPathname,
+            },
+            tokens: tokens
+        }
+
+
+    }
+
+
+
+}
---
+++ repo/server/src/domain/virtual-web/store/virtualWeb.store.ts
@@ -0,0 +1,87 @@
+
+import URLUtility from "@/utils/URL.util"
+import { WebSummary } from "@/types/WebSummary.interface"
+import PuppeterService from "@/services/puppeter.service"
+import OpenAi from "@/services/openAi.service"
+import ErrorHandler from "@/utils/errorHandler.utils"
+import VirtualWeb, { VirtualWebProps } from "../models/virtualWeb.model"
+
+const timeoutDuration = 1000 * 60 * 5 // 5M
+const maxVDomDuration = 1000 * 60 * 30 //30M
+
+class VirtualWebStore {
+
+    /**
+     * Hacer limpieza de los virtualDOM cuando tengan un tiempo sin utilizarse para ahorrar recursos.
+     */
+    private store: Map<string, VirtualWeb>
+    private timeout: NodeJS.Timeout | null = null
+    private virtualWebLastTouch = new Map<VirtualWeb, number>()
+    private openAi: OpenAi
+    private puppeteer: PuppeterService
+
+    constructor() {
+        this.store = new Map()
+        this.openAi = new OpenAi()
+        this.puppeteer = new PuppeterService()
+    }
+
+    getOrCreate({ host, mainPathname, webSummary }: VirtualWebProps) {
+
+        const getVDOM = this.store.get(host)
+
+        if (getVDOM) return getVDOM
+
+        const vdom = new VirtualWeb(this.openAi, this.puppeteer, { host, mainPathname: URLUtility.normalizePathname(mainPathname), webSummary })
+
+        return vdom
+    }
+
+    getOrThrow(host: string) {
+        const virtualWeb = this.store.get(host)
+        if (!virtualWeb) throw new ErrorHandler({
+            message: "VirtualWeb not found",
+            status_code: 404
+        })
+
+        this.touch(virtualWeb)
+        return virtualWeb
+    }
+
+    remove(host: string) {
+        this.store.delete(host)
+    }
+
+    private touch(virtualWeb: VirtualWeb) {
+        /**
+         * Basicamente se encarga de marcar el virtualWeb cuando fue la ultima vez que se utilizo,
+         * para que pueda ser limpiado si es que esta inactivo hace un tiempo x.
+         */
+        this.virtualWebLastTouch.set(virtualWeb, Date.now())
+        this.scheduleCleanup()
+    }
+
+    private scheduleCleanup() {
+        if (this.timeout !== null) return
+        this.timeout = setTimeout(() => {
+            const now = Date.now()
+            this.virtualWebLastTouch.forEach((lastTouch, vdom) => {
+                if (now - lastTouch >= maxVDomDuration) {
+                    this.virtualWebLastTouch.delete(vdom)
+                    /**
+                     * Aca tendriamos que crear un metood en el VirtualWeb que se encargue de verificar si hay algun virtualDom con un analisis activo.
+                     */
+                    this.store.delete(vdom.host)
+                    console.log(`VirtualWeb ${vdom.host} removed for inactivity`)
+                }
+            })
+            this.timeout = null
+            if (this.virtualWebLastTouch.size > 0) {
+                this.scheduleCleanup()
+            }
+        }, timeoutDuration)
+    }
+
+}
+
+export default VirtualWebStore
