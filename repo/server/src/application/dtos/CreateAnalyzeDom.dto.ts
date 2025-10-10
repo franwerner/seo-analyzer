a1a30da910c8da570e2d268c5d1431b099c454e8
Franco Werner <ifrank44445@gmail.com>
2025-10-10 13:06:03 -0300

    remove(server):Se elimina la carpeta shared y se cambia a globa-shared

--- repo/server/src/application/dtos/CreateAnalyzeDom.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationsType } from "@/domain/virtual-dom/types/ValidationType.enum"
-
-interface CreateAnalyzeDomDto {
-    path: string
-    pageSummary: string
-    validationsSelected: ValidationsType
-}
-
-export default CreateAnalyzeDomDto
--- repo/server/src/application/dtos/CreateAnalyzeSinglePage.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationsType } from "@/domain/virtual-dom/types/ValidationType.enum"
-
-interface CreateAnalyzeSinglePageDto {
-    path: string
-    host: string
-    validationsSelected: ValidationsType
-}
-
-export default CreateAnalyzeSinglePageDto
