e4b0e59f999c2fb3d657bc72a3e8b7a3e834aee2
Franco Werner <ifrank44445@gmail.com>
2025-10-10 13:07:09 -0300

    ref(server):Se elimina virtualDomUseCase y se orquesta en virtualWebUseCase

--- repo/server/src/application/dtos/CreateVirtualWeb.dto.ts
+++
@@ -1,9 +0,0 @@
-import { WebSummary } from "@/domain/virtual-web/types/WebSummary.interface"
-
-interface CreateVirtualWebDTO {
-    host: string
-    pathname: string
-    webSummary?: WebSummary | null
-}
-
-export default CreateVirtualWebDTO
