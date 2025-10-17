7f1c4fe204206807a96cdd209059f4f01af48d6f
Franco Werner <ifrank44445@gmail.com>
2025-10-17 10:23:49 -0300

    fix(web):Import

--- repo/web/src/features/virtual-web/hooks/useGetVirtualWebDetails.hook.ts
+++ repo/web/src/features/virtual-web/hooks/useGetVirtualWebDetails.hook.ts
@@ -1,10 +1,10 @@
 import { ApiErrorResponse, ApiSuccessResponse } from "@/src/common/types/ApiResponse.interface";
 import getApiResponse from "@/src/common/utils/getApiResponse.util";
-import { GetVirtualWebDetailsDTO } from "@packages/common";
+import { GetVirtualWebDetailsResponseDTO } from "@packages/common";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useGetVirtualWebDetails(virtualWebId: number) {
-    return useQuery<ApiSuccessResponse<GetVirtualWebDetailsDTO>, ApiErrorResponse>({
+    return useQuery<ApiSuccessResponse<GetVirtualWebDetailsResponseDTO>, ApiErrorResponse>({
         queryFn: async () => {
             const response = await fetch(`/api/virtual-web-stored/details/${virtualWebId}`)
             return await getApiResponse(response)
