8a8ffaa27e5834b8a5e173f97c373107c74964b4
Franco Werner <ifrank44445@gmail.com>
2025-10-20 10:01:53 -0300

    feat(server):Se quitan rutas prefijo stored

--- repo/server/src/http/routes/index.ts
+++ repo/server/src/http/routes/index.ts
@@ -1,12 +1,10 @@
 import { Express } from "express"
 import AuthRouter from "./auth.route"
 import virtualWebRouter from "./virtualWeb.route"
-import virtualWebStoredRouter from "./virtualWebStored.route"
-import virtualDomStoredRouter from "./virtualDomStored.route"
+import virtualDomRouter from "./virtualDom.route"
 
 export default function AppRoutes(app: Express) {
     app.use("/auth", AuthRouter)
     app.use("/virtual-web", virtualWebRouter)
-    app.use("/virtual-web-stored", virtualWebStoredRouter)
-    app.use("/virtual-dom-stored", virtualDomStoredRouter)
+    app.use("/virtual-dom", virtualDomRouter)
 }
