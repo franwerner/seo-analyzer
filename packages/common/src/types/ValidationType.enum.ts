1b804988131a6729112dc951677207f432769e1b
Franco Werner <ifrank44445@gmail.com>
2025-10-20 08:37:13 -0300

    update(server):Se actualizan imports

--- packages/common/src/types/ValidationType.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum ValidationTypeEnum {
-    SCHEME = "scheme",
-    SEMANTIC = "semantic",
-    SPELLING = "spelling",
-    RESOURCE = "resource",
-    STRUCTURE = "structure"
-}
-
-export type ValidationsType = Partial<Record<ValidationTypeEnum, boolean>>
