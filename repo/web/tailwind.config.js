b760aa9abc377acc5825b83da7a8263420a1430c
Franco Werner <ifrank44445@gmail.com>
2025-10-21 06:29:56 -0300

    feat(web):tailwindconfig

--- repo/web/tailwind.config.js
+++ repo/web/tailwind.config.js
@@ -4,7 +4,7 @@ import { heroui } from "@heroui/theme"
 const config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
-    "../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
